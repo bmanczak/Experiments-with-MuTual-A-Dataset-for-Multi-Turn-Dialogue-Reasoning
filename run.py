fea9fb1a3c91ccaa337f6599d7549c45945e9578
Djegodragon <49525602+LysanderdeJong@users.noreply.github.com>
2021-10-02 16:58:51 +0200

    Added support for mutual. Change --race-dir to mutual to use.
    Download dataset separately.

--- run.py
+++ run.py
@@ -192,6 +192,77 @@ def load_data(data_dir, tokenizer, max_doc_len, max_query_len, max_option_len, i
 
     return examples, features
 
+def mutual_data_loader(data_dir, tokenizer, max_doc_len, max_query_len, max_option_len, is_training):
+    """Replace the load data fucntion in run.py to use"""
+    def is_whitespace(c):
+        if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
+            return True
+        return False
+
+    if is_training:
+        subset_list = ['train']
+    else:
+        subset_list = ['dev', 'test']
+
+    examples, features = {}, {}
+    for subset in subset_list:
+        level_example_dict = {"high": None, "middle": None}
+        level_features_dict = {"high": None, "middle": None}
+
+        for level in ['high', 'middle']:
+            subset_dir = os.path.join(data_dir, subset)
+            file_list = os.listdir(subset_dir)
+            file_list = [file for file in file_list if file.endswith('txt')]
+            file_list = sorted(file_list)
+
+            alldata = []
+            for file in file_list:
+                data = json.load(open(os.path.join(subset_dir, file)))
+                alldata.append(data)
+        
+            examples_list = []
+            for data in alldata:
+                doc_id = data['id']
+                doc = data["article"].replace('\\n', '\n')
+                doc_token = []
+                prev_is_whitespace = True
+                for c in doc:
+                    if is_whitespace(c):
+                        prev_is_whitespace = True
+                    else:
+                        if prev_is_whitespace:
+                            doc_token.append(c)
+                        else:
+                            doc_token[-1] += c
+                        prev_is_whitespace = False
+
+                for i, answer in enumerate(data["answers"]):
+                    example = InputExample(
+                        guid=doc_id + '-%d' % i,
+                        doc_token=doc_token,
+                        question_text="",
+                        options=data["options"],
+                        answer=answer)
+                    examples_list.append(example)
+
+            level_example_dict[level] = examples_list
+            level_features_dict[level] = convert_examples_to_features(
+                level_example_dict[level], tokenizer, max_doc_len, max_query_len, max_option_len)
+
+        examples[subset] = level_example_dict
+        features[subset] = level_features_dict
+
+    return examples, features
+
+
+def dataset_selector(data_dir):
+    if data_dir == "RACE":
+        return load_data
+    elif data_dir == "mutual":
+        return mutual_data_loader
+    else:
+        print("dataset not implemeted.")
+        raise NotImplementedError 
 
 def accuracy(out, labels):
     outputs = np.argmax(out, axis=1)
@@ -437,10 +508,12 @@ def main():
         output_config_file = os.path.join(output_dir, CONFIG_NAME)
 
     tokenizer = BertTokenizer.from_pretrained(args.model_dir, do_lower_case=args.do_lower_case)
+    
+    load_data_fn = dataset_selector(args.race_dir)
 
     # Load and preprocess data
     if args.do_train:
-        train_examples, train_features = load_data(
+        train_examples, train_features = load_data_fn(
             args.race_dir, tokenizer, args.max_doc_len, args.max_query_len, args.max_option_len, is_training=True)
         train_examples = train_examples['train']['high'] + train_examples['train']['middle']
         train_features = train_features['train']['high'] + train_features['train']['middle']
@@ -449,7 +522,7 @@ def main():
             len(train_examples) / args.train_batch_size / args.gradient_accumulation_steps) * args.num_train_epochs
 
     if args.do_eval:
-        eval_examples, eval_features = load_data(
+        eval_examples, eval_features = load_data_fn(
             args.race_dir, tokenizer, args.max_doc_len, args.max_query_len, args.max_option_len, is_training=False)
 
     # Prepare model
